3b33ece793e1177ce2602b6324c7ea6254daba41
Patty Castaneda <Designs@pattycreates.com>
2016-01-25 00:22:53 -0800

    added example on coersion return values

--- js/front-end.js
+++ js/front-end.js
@@ -12,3 +12,17 @@ function sayHello(name) {
 }
 
 sayHello("Stacy");
+
+//Boolean with a coersion of use-alert as true or false
+function sayHello (name, useAlert) {
+	if (useAlert) {
+		alert("Hello, " + name);
+	}
+	else {
+		console.log("Hello, " + name);
+	}
+}
+//This runs statement with alert
+sayHello("Roger", true);
+//This runs statement win console since a blank parameter coerces to false
+sayHello("Clarice");
