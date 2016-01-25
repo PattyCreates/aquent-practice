920c623d2a7e31fcf82038df7ce804fe10b3ca69
Patty Castaneda <Designs@pattycreates.com>
2016-01-25 00:03:10 -0800

    initial commit

---
+++ js/front-end.js
@@ -0,0 +1,14 @@
+// Function Basics
+// Basic Function
+function sayHello() {
+	console.log("Harro!");
+}
+
+sayHello();
+
+//Basic Function with Parameter passed through 
+function sayHello(name) {
+	console.log("Harro! " + name);
+}
+
+sayHello("Stacy");
